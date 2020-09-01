68eb3f06212c412573023b96a5db33e3f57b16e5
Andrey <69449984+aakhvostov@users.noreply.github.com>
2020-09-01 08:24:09 +0300

    Add files via upload

--- 8.formats.json.xml.py
+++ 8.formats.json.xml.py
@@ -1,3 +1,7 @@
+# coding=utf-8
+from collections import Counter
+from pprint import pprint
+from collections import Counter
 import json
 import xml.etree.ElementTree as ET
 
@@ -15,12 +19,20 @@ def get_count(search_list, file_name):
         f'{sorted(count, key=count.get, reverse=True)[:10]}')
 
 
+def get_easy_count(search_list, file_name):
+    present_list = []
+    for word in Counter(xml_list).most_common(10):
+        present_list.append(word[0])
+    Counter(search_list).most_common(10)
+    return print(
+        f'10 самых часто встречающихся в новостях слов длиннее 6 символов в {file_name}- '
+        f'{present_list}')
+
+
 with open("newsafr.json", "r", encoding="utf-8") as f:
     json_list = []
     data = json.load(f)
-    rss = data.get("rss")
-    channel = rss.get("channel")
-    items = channel.get("items")
+    items = data["rss"]["channel"]["items"]
     for news in items:
         description = news.get("description")
         description_list = description.split()
@@ -35,14 +47,19 @@ tree = ET.parse("newsafr.xml", parser)
 root = tree.getroot()
 temp_xml_news = []
 xml_list = []
-xml_items = root.findall("channel/item")
+xml_items = root.findall("channel/item/description")
 for xmli in xml_items:
-    news = xmli.find("title").text
+    news = xmli.text
     temp_xml_news.append(news.split())
-    for wordls in temp_xml_news:
-        for word in wordls:
-            xml_list.append(word)
+    for words in temp_xml_news:
+        for word in words:
+            if len(word) > 6:
+                xml_list.append(word)
+xml_list.sort()
 
 
 get_count(json_list, "newsafr.json")
+get_easy_count(json_list, "newsafr.json")
+
 get_count(xml_list, "newsafr.xml")
+get_easy_count(xml_list, "newsafr.xml")
